bbdec4258cc232239b12a36128c26b51b11a149b
Surma <surma@surma.link>
2015-10-15 12:13:00 +0100

    Do DOM parsing in WebWorker

--- app/main.js
+++ app/main.js
@@ -1,16 +1,32 @@
-import {default as JSZip} from 'bower_components/jszip/dist/jszip.min';
 import {default as cssLoader} from 'modules/defer-css';
 cssLoader();
 
 const ww = new Worker('nobabel/webworker.js');
-ww.addEventListener('message', ev => {
-  console.log('Received a message');
-  console.log(ev.data);
-});
-
 const fileInput = document.querySelector('input');
 const button = document.querySelector('button');
 var file = null;
+
+ww.addEventListener('message', ev => {
+  switch (ev.data.type) {
+    case 'progress':
+      button.textContent = ev.data.message;
+    break;
+    case 'result':
+      const zipBlob = ev.data.message;
+      const blobURL = window.URL.createObjectURL(zipBlob);
+      const a = document.createElement('a');
+      a.style.display = 'none';
+      a.href = blobURL;
+      a.download = 'messages.zip';
+      document.body.appendChild(a);
+      a.click();
+      window.setTimeout(() => {
+        window.URL.revokeObjectURL(blobURL);
+        document.body.removeChild(a);
+      }, 2000);
+    break;
+  };
+});
 
 fileInput.addEventListener('change', ev => {
   if (ev.target.files.length <= 0) {
@@ -24,49 +40,5 @@ button.addEventListener('click', () => {
     return;
   }
 
-  ww.postMessage('<!doctype>');
+  ww.postMessage(file);
 });
-/*
-  const fr = new FileReader();
-  fr.addEventListener('load', ev => {
-    const doc = document.implementation.createHTMLDocument('');
-    doc.documentElement.innerHTML = ev.target.result;
-
-    const convs = doc.documentElement.querySelectorAll('.contents .thread');
-
-    // Empty container
-    const container = doc.documentElement.querySelector('.contents');
-    while (container.firstChild) {
-      container.removeChild(container.firstChild);
-    }
-
-    const zipFile = new JSZip();
-    [].forEach.call(convs, conv => {
-      const filename =
-        Array.from(conv.querySelectorAll('.user'))
-        .map(x => x.textContent)
-        .filter((value, index, array) => array.indexOf(value) === index)
-        .join('_') + '.html';
-      console.log('Processing', filename);
-
-      const newDoc = doc.cloneNode(true);
-      newDoc.documentElement.querySelector('.contents').appendChild(conv);
-      zipFile.file(filename, newDoc.documentElement.innerHTML);
-    });
-
-    const zipBlob = zipFile.generate({type: 'blob'});
-    const blobURL = window.URL.createObjectURL(zipBlob);
-    const a = document.createElement('a');
-    a.style.display = 'none';
-    a.href = blobURL;
-    a.download = 'messages.zip';
-    document.body.appendChild(a);
-    a.click();
-    window.setTimeout(() => {
-      window.URL.revokeObjectURL(blobURL);
-      document.body.removeChild(a);
-    }, 2000);
-  });
-  fr.readAsText(file, 'utf-8');
-});
-*/
--- app/modules/webworker.js
+++ app/modules/webworker.js
@@ -1,9 +1,112 @@
 import {HTMLParser} from 'modules/htmlparser';
+import {default as JSZip} from 'bower_components/jszip/dist/jszip.min';
 
-postMessage('Starting webworker');
+
 self.addEventListener('message', ev => {
-  console.log('Received a message');
-  HTMLParser(ev.data).then(dom => {
-    postMessage(dom);
+  self.postMessage({
+    type: 'progress',
+    progress: 0,
+    message: 'Reading file'
   });
-})
+  loadFile(ev.data)
+  .then(content => {
+    self.postMessage({
+      type: 'progress',
+      progress: 0,
+      message: 'Parsing file'
+    });
+    return HTMLParser(content);
+  })
+  .then(dom => {
+    self.postMessage({
+      type: 'progress',
+      progress: 0,
+      message: 'Splitting conversations'
+    });
+
+    dom = dom[0];
+    const contents = findFirstByClass.call(dom, 'contents');
+    const contentsCopy = Object.assign({}, contents);
+    contents.children = [];
+    const threads = findAllByClass.call(contentsCopy, 'thread');
+
+    return threads.map(thread => {
+      contents.children = [thread];
+      return Object.assign({}, dom);
+    });
+  }).then(threads => {
+    const zipFile = new JSZip();
+    let i = 0;
+    threads.map((conv, index, array) => {
+      self.postMessage({
+        type: 'progress',
+        progress: index / array.length,
+        message: `Serializing conversation ${index + 1} of ${array.length}`
+      });
+      return domToString(conv);
+    }).forEach((conv, index, array) => {
+      zipFile.file(`conversation_${i++}.html`, conv);
+    });
+    return zipFile;
+  }).then(zipFile => {
+    self.postMessage({
+      type: 'result',
+      message: zipFile.generate({type: 'blob'})
+    });
+  });
+});
+
+function loadFile(file) {
+  return new Promise((resolve, reject) => {
+    const fr = new FileReader();
+    fr.addEventListener('load', ev => {
+      return resolve(ev.target.result);
+    });
+    fr.readAsText(file, 'utf-8');
+  });
+}
+
+function hasClass(classname) {
+  return this.hasOwnProperty('attributes') &&
+    this.attributes.class === classname;
+}
+
+function findFirstByClass(classname) {
+  if (hasClass.call(this, classname)) {
+    return this;
+  }
+  const match = (this.children || [])
+  .map(child => findFirstByClass.call(child, classname))
+  .filter(x => !!x);
+  if (match.length == 0) {
+    return false;
+  }
+  return match[0];
+}
+
+function findAllByClass(classname) {
+  let r = [];
+  if (hasClass.call(this, classname)) {
+    r = [this];
+  }
+  return [
+    ...r,
+    ...(this.children || [])
+    .map(c => findAllByClass.call(c, classname))
+    .reduce((array, elem) => [...array, ...elem], [])
+  ];
+}
+
+function domToString(dom) {
+  if(dom.type === 'text') {
+    return dom.data;
+  }
+  if(!dom.children && dom.raw.endsWith('/')) {
+    return `<${dom.raw}>`;
+  }
+  return [
+    `<${dom.raw}>`,
+    ...(dom.children || []).map(domToString),
+    `</${dom.name}>`
+  ].join('');
+}
